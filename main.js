3a40600e413caa18b49a9e6557a40f5b45cbadcf
Colenghi <110833766+Colenghi@users.noreply.github.com>
2023-03-20 16:00:25 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,60 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+var Textbox = document.getElementById("textbox");
+
+function start()
+{
+    Textbox.innerHTML = "";
+    recognition.start();
+}
+recognition.onresult = function(event)
+{
+    console.log(event)
+    var content = event.result[0][0].transcript;
+    Textbox.innerHTML = content
+    console.log(content);
+    if (content == "selfie") 
+    {
+     console.log("tirando selfie --- ");
+     speak();
+    }
+    
+}
+function speak(){
+ var synth = window.speechSynthesis;
+ speak_data = "Tirando sua selfie em 5 segundos";  
+ var utterThis = new SpeechSynthesisUtterance(speak_data)
+ synth.speak (utterThis);
+Webcam.attach(camera);
+setTimeout(function()
+{
+    take_selfie();
+    save();
+   }, 5000);
+}
+
+ console.log ("tirando selfie ")
+
+ camera = document.getElementById("camera");
+Webcam.set({
+whidth:360,
+heigth:250,
+image_format : 'png',
+png_quality:90
+ });
+ function take_selfie() 
+ {
+    Webcam.snap(function(data_uri)
+    {
+     document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
+
+    });
+
+ }
+function save()
+{
+    link = document.getElementById("link")
+    image =  document.getElementById("selfie_image").src;
+    link.href =  image;
+    link.click();
+}
